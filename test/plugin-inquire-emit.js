bd7d8b6ae1247548c98e2de245137c74061f2cc1
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-06-13 17:56:10 +0200

    test() Install receipe globally
    Install the recipe globally in the test previous to the execution
    
    [QPA-173]

--- test/plugin-inquire-emit.js
+++ test/plugin-inquire-emit.js
@@ -1,29 +1,38 @@
 'use strict';
 
-/* global define, it, describe, before */
+/* global define, it, describe, before, after */
 const exec = require('child_process').exec;
 const execSync = require('child_process').execSync;
 const chai = require('chai');
 const expect = chai.expect;
 const recipeTestFolder = 'test/plugin-test';
+const pctp = require('pisco-callback-to-promise');
 
 describe('this.inquireEmit() addon validation', function() {
   this.timeout(60000);
 
-  before(function(done) {
-    execSync('npm install --force ../.. --loglevel=error', { cwd: recipeTestFolder });
+  before('Before script', function(done) {
+    execSync('npm install -g', {cwd: recipeTestFolder});
     done();
   });
 
   it('Should work inquiring configuration', function(done) {
     this.timeout(2000);
-    exec(`echo "valueInquire" | node ${recipeTestFolder}/bin/pisco.js context-test:test-inquire-emit --paramsFile ${recipeTestFolder}/../paramsFile/paramEmitPrompts.json`, {}, function(error, stdout, stderr) {
-      expect(error).to.equal(null);
-      expect(stderr).to.equal('');
-      expect(stdout).contain('step1:valueInquire/value2/value3');
-      expect(stdout).contain('step2:valueInquire/value2/value3');
+    pctp.c2p(exec, 'npm config get prefix', {})
+      .then((stdout) => Promise.resolve(`${stdout.trim()}/lib/node_modules/plugin-test/bin/pisco.js`))
+      .then((pisco) => {
+        pctp.c2p(exec, `echo "valueInquire" | node ${pisco} context-test:test-inquire-emit --paramsFile ${recipeTestFolder}/../paramsFile/paramEmitPrompts.json`, {})
+          .then((stdout) => {
+            expect(stdout).contain('step1:valueInquire/value2/value3');
+            expect(stdout).contain('step2:valueInquire/value2/value3');
+            done();
+          });
+      })
+      .catch(done);
+  });
 
-      done();
-    });
+  after('After script', function(done) {
+    execSync('npm uninstall -g', {cwd: recipeTestFolder});
+    done();
   });
 });
